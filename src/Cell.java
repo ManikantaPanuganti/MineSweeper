7b33b972bdd07b3deb6aa8894ae1aa6d07c720ec
manikantapanuganti <panugantimanikantachari@gmail.com>
2020-01-05 16:17:34 +0530

    created constructor in cell

--- src/Cell.java
+++ src/Cell.java
@@ -1,2 +1,9 @@
 public class Cell {
+    int x_Coordinate;
+    int y_Coordinate;
+
+    public Cell(int x_Coordinate, int y_Coordinate) {
+        this.x_Coordinate = x_Coordinate;
+        this.y_Coordinate = y_Coordinate;
+    }
 }
