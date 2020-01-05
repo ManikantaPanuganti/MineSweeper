72ca602d0eb5f2bb44130e3dfd80fab3d8fd3931
manikantapanuganti <panugantimanikantachari@gmail.com>
2020-01-05 16:42:49 +0530

    created equals method in Cell

--- src/Cell.java
+++ src/Cell.java
@@ -6,4 +6,10 @@ public class Cell {
         this.x_Coordinate = x_Coordinate;
         this.y_Coordinate = y_Coordinate;
     }
+    boolean equals(Cell cell){
+        if(cell.x_Coordinate==x_Coordinate && cell.y_Coordinate==y_Coordinate){
+            return true;
+        }
+        return false;
+    }
 }
