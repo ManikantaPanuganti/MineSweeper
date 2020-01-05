6967591c45f2bf31e2cb9c712757ce80f3b1e2f1
manikantapanuganti <panugantimanikantachari@gmail.com>
2020-01-05 16:38:53 +0530

    created selected cell method in person

--- src/Person.java
+++ src/Person.java
@@ -1,7 +1,11 @@
+import java.util.*;
 public class Person {
     String name;
     Person(String name){
         this.name=name;
     }
-
+    String selectCell(){
+        Scanner scanner=new Scanner(System.in);
+        return scanner.next();
+    }
 }
