2e4ae09a6327d37768eeb8a903112c1ba3ee9e20
manikantapanuganti <panugantimanikantachari@gmail.com>
2020-01-05 17:56:19 +0530

    created setMineFieldLayout method

--- src/Cell.java
+++ src/Cell.java
@@ -1,15 +1,25 @@
 public class Cell {
     int x_Coordinate;
     int y_Coordinate;
+    String contains;
 
-    public Cell(int x_Coordinate, int y_Coordinate) {
+    public Cell(int x_Coordinate, int y_Coordinate,String contains) {
         this.x_Coordinate = x_Coordinate;
         this.y_Coordinate = y_Coordinate;
+        this.contains=contains;
     }
     boolean equals(Cell cell){
         if(cell.x_Coordinate==x_Coordinate && cell.y_Coordinate==y_Coordinate){
             return true;
         }
         return false;
+    }
+
+    public String getContains() {
+        return contains;
+    }
+
+    public void setContains(String contains) {
+        this.contains = contains;
     }
 }
--- src/MineField.java
+++ src/MineField.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
 public class MineField {
-
+    Cell[][] mineField;
+    MineField(){
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter minefield layout: ");
+        String layout=scanner.nextLine();
+        String[] layoutArray;
+        layoutArray=layout.split(" ");
+        createLayout(layoutArray);
+    }
+    void createLayout(String[] layout){
+        mineField=new Cell[layout[0].length()][layout.length];
+        setMineFieldLayout(layout);
+    }
+    void setMineFieldLayout(String[] layout){
+        char[] symbols;
+        int row=layout.length,column=layout[0].length(),rowIndex=0,columnIndex=0;
+        for(String horizontal:layout){
+                symbols=horizontal.toCharArray();
+                for(char c:symbols){
+                    mineField[rowIndex][columnIndex]=new Cell(rowIndex,columnIndex,String.valueOf(c));
+                }
+        }
+    }
 }
