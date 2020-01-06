a7575f8d90a020a68a573aa0769f8a5027d19444
manikantapanuganti <panugantimanikantachari@gmail.com>
2020-01-06 11:11:46 +0530

    implemented isGameover and startMining method

--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -3,7 +3,27 @@ public class MineSweeper {
     Player player;
     MineSweeper(){
         mineField=new MineField();
+        player=new Player("Motu");
+    }
+    boolean isGameOver(){
+        if(mineField.isMineRevealed){
+            System.out.println("Mine opened Game is Over");
+            return true;
+        }
+        else if(mineField.isAllMinesFlagged()){
+            System.out.println("you are the winner ");
+            return true;
+        }
+        return false;
     }
     void startMining(){
+        String selectedCell;
+        boolean isOperated;
+        while(!isGameOver()){
+            mineField.displayMineField();
+            selectedCell=player.selectCell();
+            isOperated=mineField.operateCell(selectedCell);
+
+        }
     }
 }
